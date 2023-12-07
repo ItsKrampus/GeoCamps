a6806a84ddbdb756829e8d4010ded722eb4b5b78
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2023-12-07 22:41:23 +0400

    Session

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express=require('express');
 const path=require('path');
 const mongoose=require('mongoose');
-const Campground=require('./models/campground');
 const methodOverride=require('method-override');
-const ejsMate=require('ejs-mate')
-const catchAsync=require("./utils/catchAsync")
-const ExpressError=require("./utils/expressError")
-const Joi=require('joi')
-const {campgroundSchema,reviewSchema}=require('./schemas.js')
-const Review=require("./models/review.js")
-
+const ejsMate=require('ejs-mate');
+const ExpressError=require("./utils/expressError");
+const session=require('express-session');
 
 const campgrounds=require('./routes/campgrounds')
 const reviews=require('./routes/reviews')
@@ -37,6 +32,19 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+
+const sessionConfig={
+    secret:'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now()+ 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 
 
