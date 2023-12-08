93a5cb5d686e89ab18d9ad120ce5027b8daca826
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2023-12-09 01:03:10 +0400

    added registration

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const ejsMate=require('ejs-mate');
 const ExpressError=require("./utils/expressError");
 const session=require('express-session');
 const flash=require('connect-flash');
-const campgrounds=require('./routes/campgrounds')
-const reviews=require('./routes/reviews')
+const passport=require('passport');
+const LocalStrategy=require("passport-local");
+const User=require('./models/user');
+
+
+const userRoutes=require('./routes/users');
+const campgroundsRoutes=require('./routes/campgrounds');
+const reviewRoutes=require('./routes/reviews');
+
+
+
+
+
+
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/YelpCamp', {
     // useNewUrlParser: true,
@@ -48,6 +61,14 @@ app.use(session(sessionConfig));
 app.use(flash());
 
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req,res,next)=>{
     res.locals.success= req.flash('success');
     res.locals.error= req.flash('error')
@@ -55,9 +76,9 @@ app.use((req,res,next)=>{
 })
 
 
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/',userRoutes);
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req,res)=>{
     res.render('home')
