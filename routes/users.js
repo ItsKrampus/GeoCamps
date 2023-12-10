61717590bb12a91fdfa10c909f3023074ac3beb5
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2023-12-10 22:04:05 +0400

    auth updates

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const catchAsync=require('../utils/catchAsync');
 const User=require('../models/user');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req,res)=>{
     res.render('users/register')
@@ -13,8 +14,11 @@ router.post('/register',catchAsync( async(req, res)=>{
     const {email, username, password}=req.body;
     const user= new User({email, username, password});
     const registeredUser= await User.register(user, password);
-    req.flash('success', 'Welcome To Yelp Camp!');
-    res.redirect('/campgrounds');
+    req.login(registeredUser, err=>{
+        if(err) return next(err);
+        req.flash('success', 'Welcome To Yelp Camp!');
+        res.redirect('/campgrounds')
+    })
     }catch(e){
     req.flash('error', e.message);
     res.redirect('register');
@@ -25,13 +29,21 @@ router.get('/login',(req, res)=>{
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local',{failureFlash: true, failureRedirect:'/login'}), (req,res)=>{
+router.post('/login', storeReturnTo, passport.authenticate('local',{failureFlash: true, failureRedirect:'/login'}), (req,res)=>{
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds')
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl)
 })
 
-
-
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 
 module.exports=router;
