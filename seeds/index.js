e45c80aa7dd987cbb496ca9605fdfb8aa356483c
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2023-12-12 18:26:14 +0400

    review work

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDb= async()=>{
         const random1000=Math.floor(Math.random()*1000);
         const price=Math.floor(Math.random() * 20)+10;
         const camp=new campground({
+            author: '6575db544eb155a805af52a7',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: "https://source.unsplash.com/collection/483251",
