5348e3ea99af91ff8e94259756d603a2a459abba
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2023-12-14 02:33:00 +0400

    Image upload and functionallity

--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,25 @@ const seedDb= async()=>{
             author: '6575db544eb155a805af52a7',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: "https://source.unsplash.com/collection/483251",
             description:"    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, ea quia ad eum dolorum totam optio debitis nam porro assumenda reprehenderit qui minima suscipit quasi. Iure nostrum repellendus quo repudiandae?",
-            price
+            price,
+            images:[
+                {
+                    url: "https://res.cloudinary.com/dqu0ajetu/image/upload/v1702505572/YelpCamp/pexels-xue-guangjian-1687845_obvpdr.jpg",
+                    filename:"pexels-xue-guangjian-1687845_obvpdr"
+                },
+                {
+                    url:"https://res.cloudinary.com/dqu0ajetu/image/upload/v1702505574/YelpCamp/pexels-d%C6%B0%C6%A1ng-nh%C3%A2n-1328876_jtxxfd.jpg",
+                    filename:"pexels-dương-nhân-1328876_jtxxfd"
+                },
+                {
+                    url: "https://res.cloudinary.com/dqu0ajetu/image/upload/v1702505573/YelpCamp/pexels-vlad-bagacian-1061640_a4rq6x.jpg",
+                    filename:"pexels-vlad-bagacian-1061640_a4rq6x"
+                }
+
+
+            ]
+
         })
         await camp.save();
     }
